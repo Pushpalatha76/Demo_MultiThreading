7dd8c7c4e14fd2ca4d6c479fa6b3ae04906161a0
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-10-18 21:18:28 +0530

    Add files via upload

---
+++ Myclass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MultiThreading
+{
+    class Myclass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Implementing thread 1");
+
+            for(int i=0;i<100;i++)
+            {
+                Console.WriteLine("\n {0}", i);
+            }
+            Console.WriteLine("Thread 1 Closes");
+        }
+        public void Read()
+        {
+            Console.WriteLine("Implementing thread2...");
+            for (char j = 'A';j< 'Z';j++)
+                {
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Thread 2 Closes");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+
+namespace Demo_MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing Multitsking and MultiThreading in csharp!");
+            Myclass History = new Myclass();
+            Console.WriteLine("Main Thread Started.....");
+            History.Display();
+
+            ThreadStart th1 = History.Display;
+            ThreadStart th2 = History.Read;
+            Thread s1 = new Thread(th1);
+            Thread s2 = new Thread(th2);
+            
+            s1.Start();
+            s2.Start();
+
+            Console.WriteLine("Main Threads ends Here...!!!!");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
